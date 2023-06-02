746646b584e1eba65b6197235dc3168f277f5f4b
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-06-02 10:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function getFormvalue() {
+function getFormvalue(event) {
   //Write your code here
+  event.preventDefault();
 
   let fnmae = document.getElementsByTagName("input")[0].value;
   let lname = document.getElementsByTagName("input")[1].value;
 
   alert(fnmae + "   " + lname);
+
+  document.getElementById("form1").reset();
 }
